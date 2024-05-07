cfde569894d888216a32c2d159378701adf8478a
Ashish Patel <ashish.patel5@dell.com>
2024-05-07 01:20:08 +0100

    template: add debug template

---
+++ Problems/Patterns/Recursion/04-PrintAllSubsequence.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Problems.Common;
+
+namespace Problems.Patterns.Recursion
+{
+    internal class PrintAllSubsequence
+    {
+        public void Solution( int[] nums, List<int> result, int index )
+        {
+            if ( index == nums.Length )
+            {
+                TestContext.WriteLine( $"Output => {result.PrintList()}" );
+                return;
+            }
+            Solution( nums, result , index + 1 );
+            result.Add( nums[index] );
+
+            Solution( nums, result, index + 1 );
+            result.RemoveAt( result.Count - 1 );
+
+        }
+
+        [Test]
+        public void ProblemTest()
+        {
+            Solution( [3, 2, 1], new (), 0 );
+        }
+    }
+}
---
+++ Problems/Templates/DebugTemplate.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problems.Templates
+{
+    internal class DebugTemplate
+    {
+        public int Solution( int[] nums )
+        {
+            return 1;
+        }
+
+        [Test]
+        public void ProblemTest()
+        {
+            var result =  Solution( [2, 3] );
+            TestContext.WriteLine( $"Output => {result}" );
+        }
+    }
+}
